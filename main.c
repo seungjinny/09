2e6748e2258dedc1853ea555b3a47cdb22876fba
seungjinny <70585527+seungjinny@users.noreply.github.com>
2020-10-29 15:46:52 +0900

    exercise03

--- main.c
+++ main.c
@@ -6,24 +6,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i, average;
-	int sum;
+	int i;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	printf("input %i scores\n", SIZE);
-	
-	sum=0;
-	for (i=0; i<SIZE; i++)
+	for(i=0; i<SIZE; i++)
 	{
-		scanf("%d", &grade[i]);
-		sum += grade[i];
+		grade[i]= rand() % 100+1;
 	}
 	
 	for(i=0; i<SIZE; i++)
-		printf("grade[%d]=%d\n", i, grade[i]);
+		score[i] = grade[i];
 		
-	average = sum / SIZE;
-	printf("the average is: %d\n", average);
+	for(i=0; i<SIZE; i++)
+		printf("score [%d] %d (%d)\n", i, score[i], grade[i]);
 	
 	return 0;
 }
